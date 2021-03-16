eed1ed6f1b887a94b0ab361a481f4ef3346a3b1a
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-03-16 23:52:07 +0100

    Created retrofit interface and added data class for potd

--- app/src/main/java/com/example/spaceinformer/application/Constants.kt
+++ app/src/main/java/com/example/spaceinformer/application/Constants.kt
@@ -1,4 +1,10 @@
 package com.example.spaceinformer.application
 
+import com.example.spaceinformer.BuildConfig
+
 class Constants {
+
+    companion object {
+        const val API_KEY = BuildConfig.API_KEY
+    }
 }
--- app/src/main/java/com/example/spaceinformer/network/PotdService.kt
+++ app/src/main/java/com/example/spaceinformer/network/PotdService.kt
@@ -1,7 +1,12 @@
 package com.example.spaceinformer.network
 
+import com.example.spaceinformer.application.Constants
+import com.example.spaceinformer.model.Potd
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-interface PotdEndpoints {
+interface PotdService {
 
+    @GET("?api_key=${Constants.API_KEY}")
+    suspend fun getPotd():Potd
 }
