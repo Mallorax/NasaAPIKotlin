8577beef5a1d4ee0b49ed39c5afde6cfb0135aa6
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-06-30 16:50:48 +0200

    Added app bar and nav drawer

--- app/src/main/java/com/example/spaceinformer/ui/MainActivity.kt
+++ app/src/main/java/com/example/spaceinformer/ui/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.spaceinformer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.spaceinformer.R
+import com.example.spaceinformer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation(){
+        val navController = findNavController(R.id.nav_host_fragment)
+        setSupportActionBar(binding.appToolbar)
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navigateUp(navController, binding.drawerLayout)
     }
 }
