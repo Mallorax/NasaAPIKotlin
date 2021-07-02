d1589f94b5c9d9e110e3a88221d2fcf65db9c4a2
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-07-02 18:35:51 +0200

    Added thumbnails

--- app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVBindingAdapters.kt
+++ app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVBindingAdapters.kt
@@ -1,9 +1,11 @@
 package com.example.spaceinformer.ui.imagesandvideos
 
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.spaceinformer.nasapi.imagesandpictures.IvItem
+import com.squareup.picasso.Picasso
 
 @BindingAdapter("ivItemTitle")
 fun bindIvTitle(textView: TextView, ivItem: IvItem?){
@@ -12,4 +14,9 @@ fun bindIvTitle(textView: TextView, ivItem: IvItem?){
     }else{
         Log.v("TEST", "test")
     }
+}
+
+@BindingAdapter("ivThumbnail")
+fun bindIvThumbnail(imageView: ImageView, item: IvItem?){
+    Picasso.get().load(item?.imageLinks?.first()?.href).resize(240, 0).into(imageView) //TODO: Size is hardcoded and thus it is good only for mobile phones (too small for tablets)
 }
