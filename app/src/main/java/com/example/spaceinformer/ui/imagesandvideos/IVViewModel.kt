e02711d4e413c158e125454efc5b24a3f3515d9a
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-10-06 13:19:31 +0200

    ViewModel adjustments, leaving unneeded function for now

--- app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVViewModel.kt
+++ app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVViewModel.kt
@@ -9,27 +9,34 @@ import com.example.spaceinformer.repository.ivrepo.IVRetrofitRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class IVViewModel
-@Inject constructor(private val retrofitRepo: IVRetrofitRepository,
-                    private val roomFavouritesRepo: FavouritesRepo): ViewModel() {
+@Inject constructor(
+    private val retrofitRepo: IVRetrofitRepository,
+    private val roomFavouritesRepo: FavouritesRepo
+) : ViewModel() {
 
     //TODO: List for recyclerView doesn't retain data, It'd be good idea to move it here
     val mediatorLiveData = MediatorLiveData<RepositoryResponse<List<IvItem>>>()
 
-    init {
-        mediatorLiveData.addSource(getIVsFromYear(2021, 1)){
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> get() = _loading
 
-        }
-    }
+    private val _ivs = MutableLiveData<RepositoryResponse<List<IvItem>>>()
+    val ivs: LiveData<RepositoryResponse<List<IvItem>>> get() = _ivs
 
-    fun getIVs(year: Int, page: Int = 1): LiveData<RepositoryResponse<List<IvItem>>>{
+
+    fun getIVs(year: Int, page: Int = 1){
         viewModelScope.launch {
-            val response = retrofitRepo.getIVFromYearDistinct(year, page)
-            response.status = RepositoryResponse.Status.LOADING
-            
+            _loading.value = true
+            withContext(Dispatchers.IO) {
+                val response = retrofitRepo.getIVFromYearDistinct(year, page)
+                _ivs.postValue(response)
+                _loading.value = false
+            }
         }
     }
 
@@ -40,21 +47,24 @@ class IVViewModel
         }
     }
 
-    fun saveFavourite(data: Data){
-        viewModelScope.launch(Dispatchers.IO){
+    fun saveFavourite(data: Data) {
+        viewModelScope.launch(Dispatchers.IO) {
             roomFavouritesRepo.saveToFavourites(data)
         }
     }
 
-    private fun attachPage(page1: LiveData<RepositoryResponse<List<IvItem>>>,
-            page2: LiveData<RepositoryResponse<List<IvItem>>>): RepositoryResponse<List<IvItem>>{
+    private fun attachPage(
+        page1: LiveData<RepositoryResponse<List<IvItem>>>,
+        page2: LiveData<RepositoryResponse<List<IvItem>>>
+    ): RepositoryResponse<List<IvItem>> {
         val firstPage = page1.value
         val secondPage = page2.value
-        if (firstPage == null || secondPage == null){
+        if (firstPage == null || secondPage == null) {
             return RepositoryResponse.loading(null)
         }
         if (firstPage.status == RepositoryResponse.Status.ERROR ||
-            secondPage.status == RepositoryResponse.Status.ERROR){
+            secondPage.status == RepositoryResponse.Status.ERROR
+        ) {
             return RepositoryResponse.error(firstPage.message + " " + secondPage.message)
         }
         val mergedList = mutableListOf<IvItem>()
