9912476e57c80746679821d844cef221759caba8
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-10-25 17:04:37 +0200

    Binding adapter adjustment

--- app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVBindingAdapters.kt
+++ app/src/main/java/com/example/spaceinformer/ui/imagesandvideos/IVBindingAdapters.kt
@@ -4,19 +4,16 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.example.spaceinformer.model.nasapi.imagesandpictures.AppIvItem
+import com.example.spaceinformer.model.appmodels.DomainIvItem
+import com.example.spaceinformer.model.nasapi.imagesandpictures.IvItem
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("ivItemTitle")
-fun bindIvTitle(textView: TextView, appIvItem: AppIvItem?){
-    if (appIvItem != null){
-        textView.text = appIvItem.data?.first()?.title
-    }else{
-        Log.v("TEST", "test")
-    }
+fun bindIvTitle(textView: TextView, item: DomainIvItem){
+    textView.text = item.title
 }
 
 @BindingAdapter("ivThumbnail")
-fun bindIvThumbnail(imageView: ImageView, itemApp: AppIvItem?){
-    Picasso.get().load(itemApp?.imageLinks?.first()?.href).into(imageView) //TODO: Size is hardcoded and thus it is good only for mobile phones (too small for tablets)
+fun bindIvThumbnail(imageView: ImageView, item: DomainIvItem){
+    Picasso.get().load(item.imageThumbnail).into(imageView) //TODO: Size is hardcoded and thus it is good only for mobile phones (too small for tablets)
 }
